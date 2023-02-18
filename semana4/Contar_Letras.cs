57df51e774fb9ccd3baca5e71df15f9af76bf749
MorenaJv28 <124007217+MorenaJv28@users.noreply.github.com>
2023-02-17 23:29:12 -0600

    practica4

---
+++ semana4/Contar_Letras.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    
+    public class Contar_Letra
+    {
+    
+        public int Letra(string letra)
+        {
+            int numeroLetra;
+            numeroLetra = letra.Length;
+            
+            return numeroLetra;
+        }
+    }
+}
---
+++ semana4/Program.cs
@@ -0,0 +1,8 @@
+﻿using Ejercicio;
+Contar_Letra letra = new Contar_Letra();
+int LETRA;
+
+Console.WriteLine("Ingresa tu texto");
+
+LETRA = letra.Letra(Console.ReadLine());
+Console.WriteLine("Tu texto tiene " + LETRA + " Letra");
